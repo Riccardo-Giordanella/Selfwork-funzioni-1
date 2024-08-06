0016e842c5835863d5564e12cc25564a2734f26d
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-07 00:19:12 +0200

    Fine Esercizio

---
+++ main.js
@@ -0,0 +1,34 @@
+// Dichiarazione variabili
+let counter = Number(prompt("Inserisci quante volte vuoi tirare il dado:"));
+let punteggio1 = 0;
+let punteggio2 = 0;
+
+// Function lancio dadi di entrambi i giocatori
+function lancio(numero){
+    for(i = 1; i <= numero; i++){
+
+        punteggio1 = punteggio1 + Math.floor(Math.random() * (6 - 1) + 1);
+        console.log(`Al tiro del dado numero ${i}, il giocatore 1 ha compiuto ${punteggio1} punti`);
+
+        punteggio2 = punteggio2 + Math.floor(Math.random() * (6 - 1) + 1);
+        console.log(`Al tiro del dado numero ${i}, il giocatore 2 ha compiuto ${punteggio2} punti`);
+
+    }
+}
+
+// Invocazione della function
+lancio(counter)
+
+// Settaggio della if per stabilirne il vincitore
+if(punteggio1 < punteggio2){
+    console.log(`Congratulazioni, il vincitore è Giocatore 2 con il punteggio di ${punteggio2}`);
+
+}else if(punteggio1 == punteggio2){
+    console.log(`La partita è finita in parità`);
+
+}else{
+    console.log(`Congratulazioni, il vincitore è Giocatore 1 con il punteggio di ${punteggio1}`);
+
+}
+
+// Fine Esercizio
